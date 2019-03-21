e28936d6b23767d68b0f066ddeec97990d70c813
Tan Chun Siong <tanchunsiong@live.com>
2019-03-21 22:47:16 +0800

    added updateblocklist.sh

---
+++ updateblocklist.sh
@@ -0,0 +1,13 @@
+rm /home/admin/hosts
+rm /home/admin/hostsdefault
+wget '--no-check-certificate' https://raw.githubusercontent.com/tanchunsiong/customhostfiles/master/hosts -O /home/admin/hosts
+wget '--no-check-certificate' https://raw.githubusercontent.com/tanchunsiong/customhostfiles/master/hostsdefault -O /home/admin/hostsdefault
+
+rm /tmp/hostfiles/hosts
+rm /tmp/hostfiles/hostsdefault
+
+cp /home/admin/hosts /tmp/hostfiles/hosts
+cp /home/admin/hostsdefault /tmp/hostfiles/hostsdefault
+
+killall dnsmasq
+dnsmasq
